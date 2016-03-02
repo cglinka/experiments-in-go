5190eea1ba4f2b1195e9097a9bbb204eac6f4171
Clare Glinka <clare.glinka@returnpath.com>
2016-03-02 15:01:40 -0600

    Work out how to do iterate over heaps??

---
+++ heaps.go
@@ -0,0 +1,64 @@
+// This example demonstrates an integer heap built using the heap interface.
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+// An IntHeap is a min-heap of ints.
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// This example inserts several ints into an IntHeap, checks the minimum,
+// and removes them in order of priority.
+func main() {
+	h := &IntHeap{2, 1, 5}
+	fmt.Println("initial heap", h)
+	heap.Init(h)
+	fmt.Println("initalized heap", h)
+	heap.Push(h, 3)
+	fmt.Println("push 3 to heap:", h)
+	heap.Push(h, 10)
+	fmt.Println("push 10 to heap:", h)
+
+	// a := heap.Pop(h)
+	// fmt.Println(a)
+	// fmt.Println(reflect.TypeOf(a))
+	// fmt.Println(a + 5)
+	// a = heap.Pop(h).(int)
+	// fmt.Println(a)
+	// a = heap.Pop(h).(int)
+	// fmt.Println(a)
+
+	length := h.Len()
+	fmt.Println(length)
+	newThing := 5
+	for i := 0; i < length; i++ {
+		a := heap.Pop(h).(int)
+		fmt.Println("what is a?", a)
+		if a >= newThing {
+			heap.Push(h, a)
+			break
+		}
+		fmt.Println(h)
+	}
+	fmt.Println("final heap:", h)
+}
