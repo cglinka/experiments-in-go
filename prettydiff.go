5ba9133d85c8a2226d4543db88e9250929ecb3aa
Clare Glinka <clare.glinka@returnpath.com>
2018-03-14 16:34:56 -0500

    idk it was in the repo

--- prettydiff.go
+++ prettydiff.go
@@ -9,7 +9,8 @@ import (
 func main() {
 
 	a := map[string]string{"a": "a"}
-	b := map[string]string{"a": "b"}
+	b := map[string]string{"a": "a"}
 	diff := pretty.Diff(a, b)
 	fmt.Println(diff)
+	fmt.Println(len(diff))
 }
